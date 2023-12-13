9326bc7dea7fb68525ddc2a53f437d10d23f42ac
ibousv <devilsog2.0@gmail.com>
2023-12-13 21:37:37 +0000

    ex4

--- Livre.java
+++ Livre.java
@@ -3,6 +3,10 @@
     // Variables
      private String titre, auteur;
     private int nbPages;
+      private double prix = 0;
+       private boolean prixFixe = false;
+     private  int cpt = 1;
+
    // Constructeur
       public Livre(String unAuteur, String unTitre) {
         auteur = unAuteur;
@@ -21,6 +25,17 @@
         titre = unTitre;
         this.nbPages = nbPages;
       }
+
+         public Livre(String unAuteur, String unTitre, int nbPages,double unPrix) {
+            auteur = unAuteur;
+          titre = unTitre;
+           this.nbPages = nbPages;
+           prix = unPrix;
+           if (prix > 0 ) {
+               prixFixe = true ;
+           }
+      }
+      
     // Accesseur
     public String getAuteur() {
      return auteur;
@@ -34,6 +49,10 @@
         return nbPages;
      }
 
+      public double getPrix(){
+         return prix;
+      }
+
    // Modificateur
    void setNbPages(int n) {
      if (n>0) {
@@ -41,7 +60,7 @@
      }
      else
         System.out.println("Erreur le nombre de page ne peut être négatif ou nul"); 
-        System.exit(0);//permet l'arrêt du programme
+        System.exit(0);
    }
 
      void setAuteur(String  unAuteur) {
@@ -52,12 +71,31 @@
         titre = unTitre;
     }
     
+    void setPrix(double unPrix){
+       if (cpt < 2) {
+             prix = unPrix; prixFixe = true;
+             cpt++;
+        }
+        else{
+            System.out.println("Erreur, le prix ne peut être changé");
+             System.exit(0);
+        }
+      
+    }
+
     void afficheToi(){
         System.out.println(toString());
     }
     
     public String toString(){
-       String description = "Description du livre : \n Auteur = "+auteur+"\n Titre = "+titre+"\n Nombres de pages = "+nbPages;
+      String description;
+        if (prixFixe) {
+            description = "\n Description du livre : \n -> Auteur = "+getAuteur()+"\n -> Titre = "+titre+"\n -> Nombres de pages = "+nbPages+"\n Prix = fixé à "+prix+"£";
+        }
+        else{
+            description = "\n Description du livre : \n -> Auteur = "+auteur+"\n -> Titre = "+titre+"\n -> Nombres de pages = "+nbPages+"\n Prix = : pas encore fixé";
+        }
+       
        return description;
     }
 }
