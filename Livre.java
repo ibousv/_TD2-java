def226acf382f70b430f852e4d8f9b59baa424f5
ibousv <devilsog2.0@gmail.com>
2023-12-13 20:48:09 +0000

    ex2

--- Livre.java
+++ Livre.java
@@ -1,32 +1,8 @@
 
-	      /* La classe par défaut */
-/*
-    	public class Livre{
-    // Variables
-    private String titre, auteur;
-    private int nbPages
-   // Constructeur
-      public Livre(String unAuteur, String unTitre) {
-        auteur = unAuteur;
-        titre = unTitre;
-      }
-    
-    // Accesseur
-    public String getAuteur() {
-     return auteur;
-    }
-
-   // Modificateur
-   void setNbPages(int n) {
-    nbPages = nb;
-   }
-*/
-
-
        	public class Livre{
     // Variables
      private String titre, auteur;
-    private int nbPages;//Erreur no1 -> ; omise
+    private int nbPages;
    // Constructeur
       public Livre(String unAuteur, String unTitre) {
         auteur = unAuteur;
@@ -38,15 +14,44 @@
      return auteur;
     }
 
+    public String getTitre(){
+        return titre;
+    }
+     
+     public int getNbPages(){
+        return nbPages;
+     }
+
    // Modificateur
    void setNbPages(int n) {
-    nbPages = n;//Erreur no2 -> n à la place de nb
+     if (n>0) {
+         nbPages = n;
+     }
+     else
+        System.out.println("Erreur le nombre de page ne peut être négatif ou nul"); 
    }
+
+     void setAuteur(String  unAuteur) {
+      auteur = unAuteur;
+   }
+
+    void setTire(String unTitre){
+        titre = unTitre;
+    }
     
+
    public static void main(String[] args) {
    	  Livre l1 = new Livre("Sigmund Freud","Le moi et le Ça");
    	  Livre l2 = new Livre("Nietzshe","Par delà le bien et le mal");
-   	  System.out.println("L'auteur du premier livre est "+ l1.auteur);//par attribut
-      System.out.println("L'auteur du deuxième livre est "+ l2.getAuteur());//par méthode
+      //Indication des nombres de pages avec les setteurs
+      l1.setNbPages(128); l2.setNbPages(442);
+       //Affichage des nombres de pages
+   	  System.out.println("Le nombre de pages du premier livre est "+ l1.nbPages);//par attribut
+      System.out.println("Le nombre de pages du deuxième livre est "+ l2.getNbPages());//par méthode
+      //calcul du nombre de pages
+      int somme = l1.getNbPages() + l2.getNbPages();
+
+      System.out.println("Le nombre de pages de ces deux livres est "+ somme);//(l1.nbPages + l2.nbPages) est aussi possible
+
    }
 }
